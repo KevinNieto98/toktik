7bbec81fdb22033d1de516ab74a8db218f0e8b3d
Kevin Nieto <155347741+KevinNieto98@users.noreply.github.com>
2024-07-31 07:20:56 -0600

    Primeros cambios

---
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -0,0 +1,65 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:toktik/config/helpers/human_formats.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+
+class VideoButtons extends StatelessWidget {
+  final VideoPost videoPost;
+
+  const VideoButtons({super.key, required this.videoPost});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _CustomIconButton(
+          value: videoPost.likes,
+          iconData: Icons.favorite,
+          iconColor: Colors.red,
+        ),
+        const SizedBox(height: 10),
+        _CustomIconButton(
+          value: videoPost.views,
+          iconData: Icons.remove_red_eye_outlined,
+        ),
+        const SizedBox(height: 10),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton(
+            value: 0,
+            iconData: Icons.play_circle_outline,
+          ),
+        ),
+        const SizedBox(height: 20),
+      ],
+    );
+  }
+}
+
+class _CustomIconButton extends StatelessWidget {
+  final int value;
+  final IconData iconData;
+  final Color color;
+
+  const _CustomIconButton(
+      {required this.value, required this.iconData, iconColor})
+      : color = iconColor ?? Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+          icon: Icon(
+            iconData,
+            color: color,
+          ),
+          onPressed: () {},
+        ),
+        if ( value > 0 )
+          Text( HumanFormats.humanReadableNumber(value.toDouble()) ),
+      ],
+    );
+  }
+}
